27dc757f3bf51e00bc546110834085ea020ae777
55620 <cl488511534@gmail.com>
2024-11-21 11:47:27 +0800

    Update menu.sh

--- bangdingip/menu.sh
+++ bangdingip/menu.sh
@@ -13,22 +13,22 @@ read -p "请输入选项 (1/2/3): " choice
 
 case $choice in
 1)
-    echo "正在下载并运行 IP 网卡配置绑定脚本 bind.sh.x..."
-    BIND_SCRIPT_URL="https://raw.githubusercontent.com/55620/bot/9ac7cfa373b31b78bcb70bd389270353b071b1fe/bangdingip/bind.sh.x"
-    wget -O bind.sh.x $BIND_SCRIPT_URL
+    echo "正在下载并运行 IP 网卡配置绑定脚本 bind.sh..."
+    BIND_SCRIPT_URL="https://github.com/55620/bot/blob/main/bangdingip/bind.sh"
+    wget -O bind.sh $BIND_SCRIPT_URL
     if [[ $? -eq 0 ]]; then
-        chmod +x bind.sh.x
-        echo "运行 bind.sh.x 脚本中，请稍候..."
-        ./bind.sh.x
+        chmod +x bind.sh
+        echo "运行 bind.sh 脚本中，请稍候..."
+        ./bind.sh
         echo "IP 网卡配置绑定脚本运行完成！"
     else
-        echo "下载 bind.sh.x 失败，请检查下载链接是否正确！"
+        echo "下载 bind.sh 失败，请检查下载链接是否正确！"
     fi
     ;;
 2)
     echo "正在安装 sk5..."
-    SK5_FILE_URL="https://raw.githubusercontent.com/55620/bot/9ac7cfa373b31b78bcb70bd389270353b071b1fe/bangdingip/sk5"
-    SK5_SCRIPT_URL="https://raw.githubusercontent.com/55620/bot/9ac7cfa373b31b78bcb70bd389270353b071b1fe/bangdingip/sk5.sh.x"
+    SK5_FILE_URL="https://github.com/55620/bot/blob/main/bangdingip/sk5"
+    SK5_SCRIPT_URL="https://github.com/55620/bot/blob/main/bangdingip/sk5.sh"
 
     echo "下载 sk5 主文件到 /usr/local/bin..."
     wget -O /usr/local/bin/sk5 $SK5_FILE_URL
@@ -41,29 +41,29 @@ case $choice in
     fi
 
     echo "下载并运行 sk5 安装脚本..."
-    wget -O sk5.sh.x $SK5_SCRIPT_URL
+    wget -O sk5.sh $SK5_SCRIPT_URL
     if [[ $? -eq 0 ]]; then
-        chmod +x sk5.sh.x
-        echo "运行 sk5.sh.x 脚本中，请稍候..."
-        ./sk5.sh.x
+        chmod +x sk5.sh
+        echo "运行 sk5.sh 脚本中，请稍候..."
+        ./sk5.sh
         echo "sk5 安装脚本已运行完成！"
     else
-        echo "下载 sk5.sh.x 文件失败，请检查下载链接是否正确！"
+        echo "下载 sk5.sh 文件失败，请检查下载链接是否正确！"
     fi
     ;;
 3)
     echo "正在安装 l2tp..."
-    L2TP_SCRIPT_URL="https://raw.githubusercontent.com/55620/bot/44dea5380ee289dbf58dee87a170e400d98b59f9/bangdingip/1.sh.x"
+    L2TP_SCRIPT_URL="https://github.com/55620/bot/blob/main/bangdingip/1.sh"
 
     echo "下载并运行 l2tp 安装脚本..."
     wget -O 1.sh.x $L2TP_SCRIPT_URL
     if [[ $? -eq 0 ]]; then
         chmod +x 1.sh.x
-        echo "运行 1.sh.x 脚本中，请稍候..."
-        ./1.sh.x
+        echo "运行 1.sh 脚本中，请稍候..."
+        ./1.sh
         echo "l2tp 安装完成！"
     else
-        echo "下载 1.sh.x 文件失败，请检查下载链接是否正确！"
+        echo "下载 1.sh 文件失败，请检查下载链接是否正确！"
     fi
     ;;
 *)
